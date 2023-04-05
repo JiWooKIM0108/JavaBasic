5df05b193fdf6cccd51a3273244730ca09094041
JiWooKIM0108 <bbong8179@gmail.com>
2023-04-05 20:21:34 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

--- Basic/src/Lesson2/Sample3.java
+++ Basic/src/Lesson2/Sample3.java
@@ -5,7 +5,7 @@ public class Sample3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println('A');
-		System.out.println("ようこそJavaへ！");
+		System.out.println("ようこそJavaへ");
 		System.out.println(123);
 	}
 
