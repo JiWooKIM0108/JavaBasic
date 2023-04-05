0c6c5eb11b4867fbbc95e02ed113719569f6d6af
JiWooKIM0108 <bbong8179@gmail.com>
2023-04-05 20:27:44 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson3/Sample2.java
@@ -0,0 +1,16 @@
+package Lesson3;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		num = 3;
+		System.out.println("変数numの値は"+ num + "です");
+		num = 5;
+		
+		System.out.println("変数numの値を更新しました");
+		System.out.println("変数numの新しい値は"+ num + "です");
+	}
+
+}
