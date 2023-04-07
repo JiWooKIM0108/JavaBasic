69ffbcb90e6cee4bc026611a9ecd014c1ac803a2
JiWooKIM0108 <bbong8179@gmail.com>
2023-04-07 19:57:17 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson4/Sample1.java
@@ -0,0 +1,12 @@
+package Lesson4;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1+2は" + (1+2) + "です");
+		System.out.println("3*4は" + (3*4) + "です");
+		
+	}
+
+}
