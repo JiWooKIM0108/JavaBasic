b48f250d48ced9ce5cfb7a856d617ae6a8f9a2eb
JiWooKIM0108 <bbong8179@gmail.com>
2023-04-07 20:02:12 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson4/Sample2.java
@@ -0,0 +1,20 @@
+package Lesson4;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 2;
+		int num2 = 3;
+		int sum = num1+num2;
+		
+		System.out.println("変数num1の値は"+ num1 + "です");
+		System.out.println("変数num1の値は"+ num2 + "です");
+		System.out.println("num1+num2の値は"+ sum + "です");
+		
+		num1 = num1+1;
+		
+		System.out.println("変数num1の値に1をたすと" + num1 + "です");
+	}
+
+}
