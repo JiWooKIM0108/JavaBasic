3c3cdad1d5d27af2a4df738a1653d4d69b2485c2
JiWooKIM0108 <bbong8179@gmail.com>
2023-04-10 20:31:20 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson4/Sample5.java
@@ -0,0 +1,20 @@
+package Lesson4;
+
+public class Sample5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		int a = 0;
+		int b = 0;
+		
+		a = a++;
+		b = ++b;
+		
+		System.out.println("代入後にインクリメントしたのでｂの値は"+ a + "です。");
+		System.out.println("代入前にインクリメントしたのでｂの値は"+ b + "です。");
+	}
+
+}
