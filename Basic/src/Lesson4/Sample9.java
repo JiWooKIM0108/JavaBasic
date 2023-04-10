98f5fb71be119f24396949953abac8b904260414
JiWooKIM0108 <bbong8179@gmail.com>
2023-04-10 21:43:18 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson4/Sample9.java
@@ -0,0 +1,17 @@
+package Lesson4;
+
+public class Sample9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double dnum = 160.5;
+		
+		System.out.println("身長は"+ dnum + "センチです");
+		
+		System.out.println("int型の変数に代入します");
+		int inum = (int)dnum; //変換する型を指定します
+		
+		System.out.println("身長は" + inum + "センチです");
+	}
+
+}
