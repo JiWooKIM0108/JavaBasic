254b6d69110e686079280b4198dd997afb96858e
JiWooKIM0108 <bbong8179@gmail.com>
2023-04-11 20:16:00 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson4/Sample10.java
@@ -0,0 +1,14 @@
+package Lesson4;
+
+public class Sample10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d = 2;
+		double pi = 3.14;
+		
+		System.out.println("直径が" + d + "センチの円の");
+		System.out.println("円周は" + (d*pi) + "センチです");
+	}
+
+}
