4d5148a17314770b782aebf37cf62da7c13e8d47
JiWooKIM0108 <bbong8179@gmail.com>
2023-04-17 20:58:16 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson7/Sample8.java
@@ -0,0 +1,22 @@
+package Lesson7;
+
+public class Sample8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean b1 = false;
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<5; j++) {
+				if(b1 == false) {
+					System.out.println("*");
+					b1 = true;
+				} else {
+					System.out.println("-");
+					b1 = false;
+				}
+			}
+			System.out.println("\n");
+		}
+	}
+
+}
