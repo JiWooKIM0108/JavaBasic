ac794827eff422d89087e40d3ef14f1485bbeb56
JiWooKIM0108 <bbong8179@gmail.com>
2023-04-21 19:26:30 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

--- Basic/src/Lesson7/Sample3.java
+++ Basic/src/Lesson7/Sample3.java
@@ -13,7 +13,7 @@ public class Sample3 {
 		test[4] = 75;
 		
 		for(int i=0; i<5; i++) {
-			System.out.println((i+1) + "番目の人の点数は"+ test[i] + "です");
+			System.out.println((i+1) + "番目の人の点数は"+ test[i] + "です.");
 		}
 		
 	}
