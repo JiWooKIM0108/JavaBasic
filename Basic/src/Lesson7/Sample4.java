44e86ba54dcb957081193d1e82ac441ff6518344
JiWooKIM0108 <bbong8179@gmail.com>
2023-05-08 17:12:42 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson7/Sample4.java
@@ -0,0 +1,14 @@
+package Lesson7;
+
+public class Sample4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] test = {80, 60, 22, 50, 75};
+		
+		for (int i=0; i<5; i++) {
+			System.out.println((i+1) + "番目の人の点数は"+ test[i] + "です");
+		}
+	}
+
+}
