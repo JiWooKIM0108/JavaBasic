6979c38586affc19d19f04710d2eb640af0b2b66
JiWooKIM0108 <bbong8179@gmail.com>
2023-05-12 12:05:41 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson9/Sample1.java
@@ -0,0 +1,29 @@
+package Lesson9;
+
+//車クラス
+
+class Car
+{
+	int num;
+	double gas;
+	
+	void show()
+	{
+		System.out.println("車のナンバーは"+num+"です");
+		System.out.println("ガソリン量"+gas+"です");
+	}
+}
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1 = new Car();
+		
+		car1.num = 1234;
+		car1.gas = 20.5;
+		
+		car1.show();
+	}
+
+}
