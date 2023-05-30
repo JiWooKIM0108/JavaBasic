7b55737080227e74fe2b0c338211eb43143eed3e
JiWooKIM0108 <bbong8179@gmail.com>
2023-05-30 17:05:09 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson10/Sample1.java
@@ -0,0 +1,19 @@
+package Lesson10;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello";
+		
+		char ch1 = str.charAt(0);
+		char ch2 = str.charAt(1);
+		
+		int len = str.length();
+		
+		System.out.println(str + "の１番目の文字は"+ch1+"です");
+		System.out.println(str + "の2番目の文字は"+ch2+"です");
+		System.out.println(str+"の長さは"+len+"です");
+	}
+
+}
