3be48e1f797259cb222e3da16fea1a682f324556
JiWooKIM0108 <bbong8179@gmail.com>
2023-05-31 17:43:30 +0900

    JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson10/Sample5.java
@@ -0,0 +1,24 @@
+package Lesson10;
+
+import java.io.*;
+
+public class Sample5 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("整数を２つ入力してください");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str1 = br.readLine();
+		String str2 = br.readLine();
+		
+		
+		int num1 = Integer.parseInt(str1);
+		int num2 = Integer.parseInt(str2);
+		
+		int ans = Math.max(num1, num2);
+		
+		System.out.println(num1+"と"+num2+"のうち大きいほうは"+ans+"です");
+	}
+}
