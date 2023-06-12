104de4d96bcb54cf3bd6c265cc4d44bc691bb403
JiWooKIM0108 <bbong8179@gmail.com>
2023-06-12 18:35:01 +0900

    Signed-off-by: JiWooKIM0108 <bbong8179@gmail.com>

---
+++ Basic/src/Lesson11/Sample5.java
@@ -0,0 +1,15 @@
+package Lesson11;
+
+public class Sample5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car4 car1;
+		car1 = new RacingCar4();
+		
+		car1.setCar4(1234,20.5);
+		
+		car1.show();
+	}
+
+}
